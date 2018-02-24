2828b950655cbba75d003ce7a50b0f52ffb3c1fd
Mike Malcolm <mike.malcolm@Mikes-MacBook-Pro.local>
2018-02-24 18:15:09 +0000

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { FaqComponent } from './faq/faq.component';
+import { RulesComponent } from './rules/rules.component';
+import { JoinComponent } from './join/join.component';
+import { MembersComponent } from './members/members.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    AboutComponent,
+    FaqComponent,
+    RulesComponent,
+    JoinComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
